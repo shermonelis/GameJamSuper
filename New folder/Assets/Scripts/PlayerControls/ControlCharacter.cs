2a5e9bd03308bdddc78ce514ac29464421cec603
unknown <simonas@greatestgamescompany.com>
2015-01-24 00:11:11 +0200

    mp test

---
+++ New folder/Assets/Scripts/PlayerControls/ControlCharacter.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using TNet;
+
+public class ControlCharacter : TNBehaviour {
+
+
+	public static ControlCharacter instance;
+
+
+	void Awake()
+	{
+		if(TNManager.isThisMyObject)
+			instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKey(KeyCode.W))
+			transform.position += transform.forward * 5 * Time.deltaTime;
+	
+		if(Input.GetKey(KeyCode.S))
+			transform.position += transform.forward * -5 * Time.deltaTime;
+	}
+}
