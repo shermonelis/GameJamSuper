d488396034b3ecf2df3cde4072baaf2086f168e0
unknown <simonas@greatestgamescompany.com>
2015-01-24 00:00:23 +0200

    Character Test

---
+++ New folder/Assets/Scripts/PlayerControls/SpawnCharacter.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using TNet;
+
+public class SpawnCharacter : MonoBehaviour
+{
+	//
+	//
+	//
+	public void CallCreatePlayer()
+	{
+		TNManager.Create(GetComponent<TNManager>().objects[0], false);
+	}
+}
