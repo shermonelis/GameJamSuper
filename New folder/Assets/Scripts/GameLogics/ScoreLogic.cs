525c67c79d2ac2e6ad2f91ffb43fdd0a71d4d0c7
unknown <simonas@greatestgamescompany.com>
2015-01-24 17:15:07 +0200

    obelisk logix

---
+++ New folder/Assets/Scripts/GameLogics/ScoreLogic.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using TNet;
+
+public class ScoreLogic : TNBehaviour 
+{
+	void OnTriggerEnter(Collider collider)
+	{
+		if(collider.CompareTag("Part"))
+		{
+			if(collider.transform.parent != null)
+			{
+				if(collider.transform.parent.tag == gameObject.tag)
+					AddPoint();
+				else
+					RemovePoint();
+			}
+		}
+	}
+
+	public void AddPoint()
+	{
+
+	}
+
+	public void RemovePoint()
+	{
+
+	}
+}
---
+++ New folder/Assets/Scripts/Objects/Obelisk.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using TNet;
+using System.Linq;
+
+public class Obelisk : TNBehaviour {
+
+	public Color[] m_Colors = new Color[4];
+	public int m_TaskNumber = 0;
+
+	void Start()
+	{
+		Transform[] list = gameObject.transform.Find("Tasks").Cast<Transform>().ToArray();
+		for(int i = 0; i < list.Length; i++)
+		{
+			list[i].renderer.material.color = m_Colors[i];
+		}
+	}
+
+
+	void OnTriggerEnter(Collider col)
+	{
+		if(col.CompareTag("Part"))
+		{
+			if(col.renderer.material.color == m_Colors[m_TaskNumber])
+				Debug.Log("Match Color");
+			else
+			{
+				Debug.Log(col.renderer.material.color  + " " + m_Colors[m_TaskNumber]);
+			}
+		}
+
+	}
+}
