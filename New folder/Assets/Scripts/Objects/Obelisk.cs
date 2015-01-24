e9958ad6a792bf3701fca1d8c09aca871cc7f7bc
unknown <simonas@greatestgamescompany.com>
2015-01-24 18:27:22 +0200

    Obelisk

--- New folder/Assets/Scripts/Objects/Obelisk.cs
+++ New folder/Assets/Scripts/Objects/Obelisk.cs
@@ -6,14 +6,15 @@ using System.Linq;
 public class Obelisk : TNBehaviour {
 
 	public Color[] m_Colors = new Color[4];
+	public Transform[] m_Transforms = new Transform[4];
 	public int m_TaskNumber = 0;
 
 	void Start()
 	{
-		Transform[] list = gameObject.transform.Find("Tasks").Cast<Transform>().ToArray();
-		for(int i = 0; i < list.Length; i++)
+		m_Transforms = gameObject.transform.Find("Tasks").Cast<Transform>().ToArray();
+		for(int i = 0; i < m_Transforms.Length; i++)
 		{
-			list[i].renderer.material.color = m_Colors[i];
+			m_Transforms[i].renderer.material.color = m_Colors[i];
 		}
 	}
 
@@ -23,12 +24,36 @@ public class Obelisk : TNBehaviour {
 		if(col.CompareTag("Part"))
 		{
 			if(col.renderer.material.color == m_Colors[m_TaskNumber])
-				Debug.Log("Match Color");
-			else
-			{
-				Debug.Log(col.renderer.material.color  + " " + m_Colors[m_TaskNumber]);
-			}
+				AddPoint(col.gameObject);
 		}
+	}
 
+	void OnTriggerExit(Collider col)
+	{
+		if(col.CompareTag("Part"))
+		{
+			int index = 0;
+			for(int i = 0; i < m_Colors.Length; i++)
+			{
+				if(col.renderer.material.color == m_Colors[i])
+				{
+					index = i;
+				}
+			}
+			if(index < m_TaskNumber)
+				RemovePoint();
+		}
+	}
+
+	private void RemovePoint()
+	{
+		m_TaskNumber--;
+	}
+
+	private void AddPoint(GameObject g)
+	{
+		g.transform.position = m_Transforms[m_TaskNumber].position;
+		g.transform.parent = m_Transforms[m_TaskNumber];
+		m_TaskNumber++;
 	}
 }
