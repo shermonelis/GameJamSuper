7cae8ba49864d1ef897e3993fa0eae08f1d1cb4b
Darth-Carrotpie <carrotpie@gmail.com>
2015-01-25 14:33:12 +0200

    Sound Fix

--- New folder/Assets/Scripts/GameLogics/ScoreLogic.cs
+++ New folder/Assets/Scripts/GameLogics/ScoreLogic.cs
@@ -10,12 +10,12 @@ public class ScoreLogic : TNBehaviour
 	public GUISkin m_Skin;
 	public AudioClip winSound;
 	public AudioClip[] looseSound;
-	void Start()
-	{
+	bool m_soundTrigger = false;
+	void Start() {
+		m_soundTrigger=true;
 		m_Blue = GameObject.Find("Obelisk_Blue").GetComponent<Obelisk>();
 		m_Red = GameObject.Find("Obelisk_Red").GetComponent<Obelisk>();
 	}
-
 
 	void OnGUI()
 	{
@@ -26,21 +26,26 @@ public class ScoreLogic : TNBehaviour
 		GUI.color = Color.yellow;
 
 		GUI.Label(new Rect(Screen.width/2 - 50, 15, 100, 50), m_Red.m_TaskNumber + "-" + m_Blue.m_TaskNumber);
-
 		
 		if(m_Blue.m_TaskNumber >= 4){
 			GUI.Box(new Rect(Screen.width/2 - 640, Screen.height/2 - 360, 1280, 720), "", "BlueWins");
-			if(PlayerInfo.instance.m_Team==0)
-				audio.PlayOneShot(winSound);
-			else
-				audio.PlayOneShot(looseSound[Mathf.FloorToInt(Random.Range(0, 1.9f))]);
+			if(m_soundTrigger){
+				m_soundTrigger=false;
+				if(PlayerInfo.instance.m_Team==0)
+					audio.PlayOneShot(winSound);
+				else
+					audio.PlayOneShot(looseSound[Mathf.FloorToInt(Random.Range(0, 1.9f))]);
+			}
 		}
 		if(m_Red.m_TaskNumber >= 4){
 			GUI.Box(new Rect(Screen.width/2 - 640, Screen.height/2 - 360, 1280, 720), "", "RedWins");
-			if(PlayerInfo.instance.m_Team==1)
-				audio.PlayOneShot(winSound);
-			else
-				audio.PlayOneShot(looseSound[Mathf.FloorToInt(Random.Range(0, 1.9f))]);
+			if(m_soundTrigger){
+				m_soundTrigger=false;
+				if(PlayerInfo.instance.m_Team==1)
+					audio.PlayOneShot(winSound);
+				else
+					audio.PlayOneShot(looseSound[Mathf.FloorToInt(Random.Range(0, 1.9f))]);
+			}
 		}
 	}
 }
